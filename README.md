8bfed062cd3c86cc76dcb518fa9ea5eb932d83c8
Tomi Adenekan <tominekan@outlook.com>
2021-08-23 12:39:22 -0700

    Iniital commit, created first api call

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fur
-Command line downloads manager that leverages libcurl (WIP)
+Command line downloads manager that leverages libcurl and textual (WIP)
+It uses python as a frontend and C as the backend
+This is important for coding
---
+++ src/fur_backend.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+
+static size_t write_data(void *ptr, size_t size, size_t num_members, void *stream) {
+    size_t written = fwrite(ptr, size, num_members, (FILE *) stream);
+    return written;
+}
+
+int download_file(char *url, char *path) {
+    CURL *curl_handle; 
+    FILE *filehandle;
+ 
+    curl_global_init(CURL_GLOBAL_ALL);
+    
+    // Initialize this curl session
+    curl_handle = curl_easy_init();
+
+    // set the link to download 
+    curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+
+    // Don't show any info 
+    curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 0L);
+
+    // Disable progress meter
+    curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+
+    // Send all data to the write_data() function
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+
+    // open the file
+    filehandle = fopen(path, "wb");
+    if (filehandle) {
+        // Write the content to the file handle
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, filehandle);
+
+        // Get the file
+        curl_easy_perform(curl_handle);
+
+        // close the file
+        fclose(filehandle);
+    }
+
+    // Cleanup
+    curl_easy_cleanup(curl_handle);
+    curl_global_cleanup();
+
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv) {
+    download_file("https://google.com", "index.html");
+}
